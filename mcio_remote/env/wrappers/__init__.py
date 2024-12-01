a2ff6550db3b71c745638e238d2a3ba40eff333c
TwoTurtles <97465192+twoturtles@users.noreply.github.com>
2024-11-30 22:57:12 -0500

    delete env wrappers that were auto added by the template system
    ruff is detecting errors and they're not used

--- mcio_remote/env/wrappers/__init__.py
+++
@@ -1,4 +0,0 @@
-from mcio_env.wrappers.clip_reward import ClipReward
-from mcio_env.wrappers.discrete_actions import DiscreteActions
-from mcio_env.wrappers.reacher_weighted_reward import ReacherRewardWrapper
-from mcio_env.wrappers.relative_position import RelativePosition
