91566852ae2d8a3be1d1de648c13f2def0522bd7
TwoTurtles <97465192+twoturtles@users.noreply.github.com>
2024-12-03 23:12:48 -0500

    new tests

--- tests/test_start.py
+++ tests/test_start.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pytest
 from mcio_remote.mcio_env.envs import mcio_env
+from mcio_remote import network
 
 
 @pytest.fixture
@@ -9,24 +10,38 @@ def default_mcio_env():
 
 
 @pytest.fixture
-def action_space_dict1():
+def action_space_sample1():
     return {
-        "cursor_pos_rel": np.array([-637.4071, 344.94278], dtype=np.float32),
-        "keys": {"32": 2, "340": 2, "65": 1, "68": 0, "83": 1, "87": 1},
-        "mouse_buttons": {"0": 1, "1": 2},
+        "cursor_pos_rel": np.array([827.648, 22.274418], dtype=np.float32),
+        "keys": {"A": 0, "D": 0, "E": 1, "L_SHIFT": 1, "S": 1, "SPACE": 1, "W": 1},
+        "mouse_buttons": {"LEFT": 1, "RIGHT": 0},
     }
 
 
-def test_fixture_is_valid(default_mcio_env, action_space_dict1):
-    assert action_space_dict1 in default_mcio_env.action_space
+def test_fixture_is_valid(default_mcio_env, action_space_sample1):
+    assert action_space_sample1 in default_mcio_env.action_space
 
 
-def test_key_space_to_pairs1(action_space_dict1):
-    pairs = mcio_env._key_space_to_pairs(action_space_dict1["keys"])
-    assert pairs == [(65, 1), (68, 0), (83, 1), (87, 1)]
+def test_action_to_packet(default_mcio_env, action_space_sample1):
+    expected1 = network.ActionPacket(
+        version=0,
+        sequence=0,
+        commands=[],
+        keys=[(32, 1), (69, 1), (83, 1), (87, 1), (340, 1)],
+        mouse_buttons=[(0, 1)],
+        cursor_pos=[(827, 22)],
+    )
+    pkt = default_mcio_env._action_to_packet(action_space_sample1)
+    assert pkt == expected1
 
-
-def test_mb_space_to_pairs1(action_space_dict1):
-    pairs = mcio_env._mb_space_to_pairs(action_space_dict1["mouse_buttons"])
-    print(pairs)
-    assert pairs == [(0, 1)]
+    expected2 = network.ActionPacket(
+        version=0,
+        sequence=0,
+        commands=[],
+        keys=[],
+        mouse_buttons=[],
+        cursor_pos=[(827, 22)],
+    )
+    # Passing the same action. Keys and mouse_buttons should be cleared since they're already set.
+    pkt = default_mcio_env._action_to_packet(action_space_sample1)
+    assert pkt == expected2
