f84effc39ccfbd1a099b247b42fb44b5a864ffef
TwoTurtles <97465192+twoturtles@users.noreply.github.com>
2024-12-04 09:12:50 -0500

    mypy - ignore glfw and OpenGL for now

--- mcio_remote/gym_lite.py
+++ mcio_remote/gym_lite.py
@@ -25,7 +25,7 @@ class GymLite:
         self._window_width = None
         self._window_height = None
 
-    def reset(self, commands: List[str] | None = None) -> None:
+    def reset(self, commands: List[str] | None = None) -> network.ObservationPacket:
         if commands is None:
             commands = []
         if self.render_mode == "human":
