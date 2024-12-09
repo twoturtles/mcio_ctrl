1f019d4905b252c19970caa7e120e4ac842be0ba
TwoTurtles <97465192+twoturtles@users.noreply.github.com>
2024-12-08 23:18:01 -0500

    hatch does not like an f-string version

--- mcio_remote/__about__.py
+++ mcio_remote/__about__.py
@@ -2,4 +2,4 @@ from typing import Final
 
 MCIO_PROTOCOL_VERSION: Final[int] = 0
 # mcio_remote version + MCio Protocol Version
-__version__ = f"v0.1.1+p{MCIO_PROTOCOL_VERSION}"
+__version__ = "v0.1.1+p0"
