768b3ade25fd635673c6585cd8a23030191f601e
TwoTurtles <97465192+twoturtles@users.noreply.github.com>
2024-12-17 16:46:08 -0500

    working on world gen

--- mcio_remote/launcher.py
+++ mcio_remote/launcher.py
@@ -5,6 +5,9 @@ from dataclasses import dataclass, asdict, field
 import uuid
 from pathlib import Path
 import os
+import random
+import sys
+import shutil
 from typing import Any, Final, Literal, Optional, TypeAlias
 
 from tqdm import tqdm
@@ -29,10 +32,6 @@ DEFAULT_WINDOW_HEIGHT: Final[int] = 480
 MCIO_MODE = Literal["off", "async", "sync"]
 
 REQUIRED_MODS: list[str] = ["fabric-api", "mcio"]
-
-# TODO
-# multiple instances
-# create world: name, seed, mode, difficulty, ...
 
 
 class Launcher:
@@ -170,7 +169,8 @@ class Installer:
 
         # Download the server to use for world generation
         print("Installing server.jar")
-        download_server(self.mc_dir, self.mc_version)
+        server = Server(self.mc_dir)
+        server.install_server(self.mc_version)
 
         # Disable narrator
         opts = OptionsTxt(self.mc_dir / "options.txt")
@@ -208,7 +208,7 @@ class Installer:
         filename = jar_info["filename"]
 
         mods_dir = mc_dir / "mods"
-        mods_dir.mkdir(exist_ok=True)
+        mods_dir.mkdir(parents=True, exist_ok=True)
         print(f"Installing {filename}")
         with open(mods_dir / filename, "wb") as f:
             f.write(response.content)
@@ -252,17 +252,26 @@ class _InstallProgress:
 
 
 class OptionsTxt:
-    """Load/Save options.txt. Keeps everything as strings."""
+    """Load/Save options.txt. Keeps everything as strings.
+    To work with server.properties instead, set separator to "="
+    """
 
-    def __init__(self, options_path: Path | str) -> None:
+    def __init__(
+        self, options_path: Path | str, separator: Literal[":", "="] = ":"
+    ) -> None:
         self.path = Path(options_path).expanduser()
-        self.options = self._load(self.path)
+        self.sep = separator
+        self.options = self._load()
 
     def save(self) -> None:
         """Save options back to file"""
         with self.path.open("w") as f:
             for key, value in self.options.items():
-                f.write(f"{key}:{value}\n")
+                f.write(f"{key}{self.sep}{value}\n")
+
+    def clear(self) -> None:
+        """Clear the file"""
+        self.options = {}
 
     def __getitem__(self, key: str) -> str:
         return self.options[key]
@@ -270,24 +279,172 @@ class OptionsTxt:
     def __setitem__(self, key: str, value: str) -> None:
         self.options[key] = value
 
-    def _load(self, options_path: Path) -> dict[str, str]:
-        """Load options from file"""
+    def _load(self) -> dict[str, str]:
+        """Load options from file. Returns as dict."""
         if not self.path.exists():
             return {}
 
         with self.path.open("r") as f:
             txt = f.read()
-        lines = txt.strip().split()
+        lines = txt.strip().split("\n")
         options = {}
         for line in lines:
             line = line.strip()
             if len(line) == 0 or line.startswith("#"):
                 continue
-            key, value = line.split(":", 1)
+            key, value = line.split(self.sep, 1)
             key = key.strip()
             value = value.strip()
             options[key] = value
         return options
+
+
+class Server:
+    """Install / interface with Minecraft server."""
+
+    SERVER_SUBDIR = "server"
+
+    def __init__(self, mc_dir: Path) -> None:
+        self.mc_dir = mc_dir
+        self.server_dir = self.mc_dir / self.SERVER_SUBDIR
+        self.server_dir.mkdir(parents=True, exist_ok=True)
+        self._process: subprocess.Popen[str] | None = None
+
+    def install_server(self, mc_version: str) -> None:
+        info = get_version_details(mc_version)
+        server_url = info["downloads"]["server"]["url"]
+
+        response = requests.get(server_url)
+        response.raise_for_status()
+
+        with open(self.server_dir / "server.jar", "wb") as f:
+            f.write(response.content)
+        self._write_eula()
+
+    def set_server_properties(
+        self, properties: dict[str, str], clear: bool = False
+    ) -> None:
+        if clear:
+            self.clear_server_properties()
+        opts = self._load_properties()
+
+        for key, value in properties.items():
+            opts[key] = value
+        opts.save()
+
+    def clear_server_properties(self) -> None:
+        opts = self._load_properties()
+        opts.clear()
+        opts.save()
+
+    def set_server_property(self, key: str, value: str) -> None:
+        self.set_server_properties({key: value})
+
+    def run(self) -> None:
+        """This will generate the world. Make sure server.properties are set first."""
+        assert self._process is None
+        cmd = self.get_start_command()
+        self._process = subprocess.Popen(
+            cmd,
+            cwd=self.server_dir,
+            stdin=subprocess.PIPE,
+            # stdout=subprocess.PIPE,
+            # stderr=subprocess.PIPE,
+            text=True,
+        )
+
+    def send_command(self, command: str) -> None:
+        """Send a command to a running server."""
+        assert self._process is not None
+        assert self._process.stdin is not None  # For mypy
+        if not command.endswith("\n"):
+            command += "\n"
+        self._process.stdin.write(command)
+        self._process.stdin.flush()
+
+    def stop(self) -> None:
+        """Tell server to exit."""
+        assert self._process is not None
+        self.send_command("stop")
+        self._process.wait()
+        self._process = None
+
+    def get_start_command(self) -> list[str]:
+        """Get the shell command to start the server."""
+        # XXX What should java version be?
+        java_cmd = mll.runtime.get_executable_path("java-runtime-delta", self.mc_dir)
+        if java_cmd is None:
+            raise ValueError("Error getting java command")
+        cmd = [java_cmd]
+        # Args from https://www.minecraft.net/en-us/download/server
+        server_args = "-Xmx1024M -Xms1024M -jar server.jar nogui".split()
+        return cmd + server_args
+
+    def _write_eula(self) -> None:
+        with open(self.server_dir / "eula.txt", "w") as f:
+            f.write("eula=true\n")
+
+    def _load_properties(self) -> OptionsTxt:
+        return OptionsTxt(self.server_dir / "server.properties", separator="=")
+
+
+class WorldGen:
+    """
+    Use this interface for generating worlds (vs. using the Server directly)
+    https://minecraft.fandom.com/wiki/Server.properties
+    """
+
+    def __init__(
+        self,
+        instance_id: "InstanceID",
+        mcio_dir: Path | str | None = None,
+    ) -> None:
+        """
+        You can set gamemode, difficult, and level-seed via function arguments or server_properties.
+        Values in server_properties will override function arguments.
+        """
+        self.instance_id = instance_id
+        mcio_dir = mcio_dir or DEFAULT_MCIO_DIR
+        self.mcio_dir = Path(mcio_dir).expanduser()
+
+    def generate(
+        self,
+        gamemode: Literal[
+            "survival", "creative", "adventure", "spectator"
+        ] = "survival",
+        difficulty: Literal["peaceful", "easy", "normal", "hard"] = "normal",
+        seed: int | str | None = None,
+        reset_server_properties: bool = False,
+        server_properties: dict[str, str] | None = None,
+    ) -> None:
+        """This assumes the server has already been installed"""
+        if seed is None:
+            seed = random.randint(0, sys.maxsize)
+        seed = str(seed)
+
+        self.mc_dir = get_minecraft_dir(self.mcio_dir, self.instance_id)
+        self.server = Server(self.mc_dir)
+
+        # Clear the world dir before generation
+        world_dir = self.server.server_dir / "world"
+        if world_dir.exists():
+            shutil.rmtree(world_dir)
+
+        server_properties = server_properties or {}
+        default_properties = {
+            "gamemode": gamemode,
+            "difficulty": difficulty,
+            "level-seed": seed,
+        }
+        server_properties = default_properties | server_properties
+        self.server.set_server_properties(
+            server_properties, clear=reset_server_properties
+        )
+        # After stop the world dir should be ready
+        print("Starting world generation...")
+        self.server.run()
+        self.server.stop()
+        print("Done")
 
 
 ##
@@ -408,16 +565,3 @@ def get_version_details(mc_version: str) -> dict[str, Any]:
     response.raise_for_status()
     ver_details: dict[str, Any] = response.json()
     return ver_details
-
-
-def download_server(mc_dir: Path, mc_version: str) -> None:
-    info = get_version_details(mc_version)
-    server_url = info["downloads"]["server"]["url"]
-
-    response = requests.get(server_url)
-    response.raise_for_status()
-
-    server_dir = mc_dir / "server"
-    server_dir.mkdir(exist_ok=True)
-    with open(server_dir / "server.jar", "wb") as f:
-        f.write(response.content)
