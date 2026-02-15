d604aa355d4087231c3cda67b9a5357f19fa13a0
TwoTurtles <97465192+twoturtles@users.noreply.github.com>
2026-02-14 19:25:01 -0500

    add try/finally to make sure env gets closed

--- tests/integration/test_minerl_env.py
+++ tests/integration/test_minerl_env.py
@@ -12,22 +12,22 @@ from .conftest import get_test_run_options
 def test_minerl_env(minecraft_session: None) -> None:
     run_opts = get_test_run_options(launch=False)
     env = minerl_env.MinerlEnv(run_opts)
+    try:
+        # Note: tp is absolute yaw, pitch vs. camera which is Δ-pitch, Δ-yaw
+        setup_commands = [
+            # /tp <target> <x> <y> <z> <yaw> <pitch>
+            "teleport @s ~ ~ ~ -30 50",
+        ]
+        obs, info = env.reset(options={"commands": setup_commands})
+        obs, reward, terminated, truncated, info = env.skip_steps(25)
+        assert env.last_pitch == pytest.approx(50.0, abs=1.0)
+        assert env.last_yaw == pytest.approx(-30.0, abs=1.0)
 
-    # Note: tp is absolute yaw, pitch vs. camera which is Δ-pitch, Δ-yaw
-    setup_commands = [
-        # /tp <target> <x> <y> <z> <yaw> <pitch>
-        "teleport @s ~ ~ ~ -30 50",
-    ]
-    obs, info = env.reset(options={"commands": setup_commands})
-    obs, reward, terminated, truncated, info = env.skip_steps(25)
-    assert env.last_pitch == pytest.approx(50.0, abs=1.0)
-    assert env.last_yaw == pytest.approx(-30.0, abs=1.0)
-
-    action = env.get_noop_action()
-    # Delta pitch, yaw
-    action["camera"] = np.array((-10, 20), dtype=np.float32)
-    obs, reward, terminated, truncated, info = env.step(action)
-    assert env.last_pitch == pytest.approx(40.0, abs=1.0)
-    assert env.last_yaw == pytest.approx(-10.0, abs=1.0)
-
-    env.close()
+        action = env.get_noop_action()
+        # Delta pitch, yaw
+        action["camera"] = np.array((-10, 20), dtype=np.float32)
+        obs, reward, terminated, truncated, info = env.step(action)
+        assert env.last_pitch == pytest.approx(40.0, abs=1.0)
+        assert env.last_yaw == pytest.approx(-10.0, abs=1.0)
+    finally:
+        env.close()
